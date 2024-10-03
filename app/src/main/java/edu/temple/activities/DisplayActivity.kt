0b48d285d5666a1e552f1525f702e26d33358504
Norahs4869 <tul29737@temple.edu>
2024-10-03 14:45:03 -0400

    Created the launcher for TextSizeActivity so that when button is clicked it launches the TextSizeActivity

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,13 +1,25 @@
 package edu.temple.activities
 
+import android.app.LauncherActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
+const val RESULT_KEY = "reply_message"
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
+    private var launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK){
+            it.data?.apply{
+                lyricsDisplayTextView.text = getStringExtra(RESULT_KEY)
+            }
+        }
+
+    }
 
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
 
@@ -21,5 +33,9 @@ class DisplayActivity : AppCompatActivity() {
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
 
+        textSizeSelectorButton.setOnClickListener(){
+            val launchIntent = Intent(this@DisplayActivity, TextSizeActivity::class.java)
+            launcher.launch(launchIntent)
+        }
     }
 }
